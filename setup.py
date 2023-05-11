fdaa565fb1f18d267248e6809ae8911f1fdc9f2d
pciturri <pciturri@gfz-potsdam.de>
2023-05-11 14:58:28 +0200

    style: renamed folders and packages to pymock build: now uses setup.cfg to install python pkg. Updated requirements and setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-
-setup(
-    name='pymock',                  # Model name
-    version='0.1.0',                
-    author='Pablo Iturrieta',       
-    author_email='pciturri@gfz-potsdam.de',
-    license='LICENSE',
-    description='Mock-up model for the Italy Experiment',
-    install_requires=['numpy==1.21.5', 'matplotlib==3.4.3', 'pytest'],  # << Dependencies
-    entry_points={
-        # Usage to define a binary entry point. Example:
-        # 'console_scripts': [$binary_name = $module:$function]
-        'console_scripts': ['run = run:run']
-    },
-    packages=find_packages(),
-    include_package_data=True,
-    python_requires=">=3.7",
-    url='https://git.gfz-potsdam.de/csep-group/rise_italy_experiment/models/pymock'
-)
+if __name__ == '__main__':
+    setup()
